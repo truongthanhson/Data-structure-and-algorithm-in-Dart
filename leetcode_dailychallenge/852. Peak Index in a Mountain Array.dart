fcc351b793e58c66fdbe12ddf024a9c0219793c3
Son Truong <son.truong@oivan.com>
2023-07-25 11:01:19 +0700

    add: leetcode daily challenge 25/07/2023

---
+++ leetcode_dailychallenge/852. Peak Index in a Mountain Array.dart
@@ -0,0 +1,25 @@
+void main(List<String> args) {
+  print(Solution().peakIndexInMountainArray([0, 1, 0]));
+  print(Solution().peakIndexInMountainArray([0, 2, 1, 0]));
+  print(Solution().peakIndexInMountainArray([0, 10, 5, 2]));
+  print(Solution()
+      .peakIndexInMountainArray([18, 29, 38, 59, 98, 100, 99, 98, 90]));
+  print(Solution().peakIndexInMountainArray([0, 10, 5, 2]));
+}
+
+class Solution {
+  int peakIndexInMountainArray(List<int> arr) {
+    var l = 0, r = arr.length - 1;
+    while (l < r) {
+      int mid = l + (r - l + 1) ~/ 2;
+      if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1]) {
+        return mid;
+      } else if (arr[mid - 1] < arr[mid]) {
+        l = mid;
+      } else {
+        r = mid - 1;
+      }
+    }
+    return l;
+  }
+}
